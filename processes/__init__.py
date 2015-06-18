aa9a95f83cb1c160d72c818b4b611ac2d4539e7a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-06-18 17:31:45 +0200

    process multiplesources added

--- processes/__init__.py
+++ processes/__init__.py
@@ -4,4 +4,5 @@ __all__ = [
            "wps_answertolife",
            "wps_wordcounter",
            "wps_inout",
+           "wps_multiplesources",
     ]
---
+++ processes/wps_multiplesources.py
@@ -0,0 +1,59 @@
+from malleefowl.process import WPSProcess
+
+from malleefowl import wpslogging as logging
+logger = logging.getLogger(__name__)
+
+
+class MultipleSource(WPSProcess):
+    """
+    Has multiple different sources
+    """
+    def __init__(self):
+        WPSProcess.__init__(
+            self,
+            identifier="multiplesources", 
+            title="Multiple Sources",
+            version = "1.0",
+            abstract="Process with multiple different sources ...",
+            )
+
+        self.model_data = self.addComplexInput(
+            identifier = "model",
+            title = "Model data",
+            abstract = "URL of NetCDF model data file",
+            minOccurs=0,
+            maxOccurs=100,
+            formats=[{"mimeType":"application/netcdf"}],
+            maxmegabites=100,
+            )
+
+        self.obs_data = self.addComplexInput(
+            identifier = "obs",
+            title = "Observational data",
+            abstract = "URL of NetCDF observational data file",
+            minOccurs=0,
+            maxOccurs=100,
+            formats=[{"mimeType":"application/netcdf"}],
+            maxmegabites=100,
+            )
+        
+        self.output = self.addComplexOutput(
+            identifier = "output",
+            title = "Model comparison result",
+            formats=[{"mimeType":"text/plain"}],
+            asReference=True,
+            )
+                                           
+    def execute(self):
+        self.show_status("Starting ...", 0)
+
+        model_files = self.getInputValues(identifier='model_data')
+        obs_files = self.getInputValues(identifier='obs_data')
+
+        outfile = self.mktempfile(suffix='.txt')
+        with open(outfile, 'w') as fout: 
+            fout.write('Comparing {0} model files with {0} obs files\n\n'.format(len(model_files), len(obs_files)))
+            self.output.setValue( fout.name )
+
+        self.show_status("Done", 100)
+
