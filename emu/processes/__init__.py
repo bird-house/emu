f04689296ff52ba1708430a7ff4999ab66784ee1
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-01 18:20:07 +0100

    disabled muliplerecources process

--- emu/processes/__init__.py
+++ emu/processes/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     "wps_dummyprocess",
     "wps_wordcounter",
     "wps_inout",
-    "wps_multiplesources",
+    #"wps_multiplesources",
     "wps_chomsky",
     "wps_zonalmean"
     ]
