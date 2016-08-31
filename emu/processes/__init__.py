0f2df9e040ee50b3b3f12daa88fa18b2deeb1598
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-31 19:20:56 +0200

    added sleep process

--- emu/processes/__init__.py
+++ emu/processes/__init__.py
@@ -1,16 +1,5 @@
+from .wps_sleep import Sleep
 
-__all__ = [
-    "wps_helloworld",
-    #"wps_answertolife",
-    "wps_dummyprocess",
-    "wps_wordcounter",
-    "wps_inout",
-    "wps_bbox",
-    #"wps_multiplesources",
-    "wps_chomsky",
-    #"wps_zonalmean"
-    ]
-
-from emu.processes.wps_answertolife import UltimateQuestionProcess
-
-#__processes__ = [UltimateQuestionProcess()]
+processes = [
+    Sleep(),
+]
