cdd743624aafab3c58090b230bb87a807397176e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-09-01 13:47:47 +0200

    renamed dummy process

--- emu/processes/__init__.py
+++ emu/processes/__init__.py
@@ -2,7 +2,7 @@ from .wps_sleep import Sleep
 from .wps_ultimate_question import UltimateQuestion
 from .wps_bbox import Box
 from .wps_helloworld import HelloWorld
-from .wps_dummyprocess import Dummy
+from .wps_dummy import Dummy
 
 processes = [
     UltimateQuestion(),
