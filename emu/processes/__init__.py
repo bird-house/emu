c6851609f9d373821e061b155081601eb3bc7c4e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-09-01 13:44:45 +0200

    update dummy process

--- emu/processes/__init__.py
+++ emu/processes/__init__.py
@@ -2,10 +2,12 @@ from .wps_sleep import Sleep
 from .wps_ultimate_question import UltimateQuestion
 from .wps_bbox import Box
 from .wps_helloworld import HelloWorld
+from .wps_dummyprocess import Dummy
 
 processes = [
     UltimateQuestion(),
     Sleep(),
     Box(),
     HelloWorld(),
+    Dummy(),
 ]
--- emu/processes/wps_dummyprocess.py
+++ emu/processes/wps_dummyprocess.py
@@ -3,33 +3,41 @@ DummyProcess to check the WPS structure
 
 Author: Jorge de Jesus (jorge.jesus@gmail.com) as suggested by Kor de Jong
 """
-from pywps.Process import WPSProcess
-import types
-class Process(WPSProcess):
-     def __init__(self):
-          # init process
-         WPSProcess.__init__(self,
-              identifier = "dummyprocess", # must be same, as filename
-              title="Dummy Process",
-              version = "1.0",
-              storeSupported = "true",
-              statusSupported = "true",
-              abstract="The Dummy process is used for testing the WPS structure. The process will accept 2 input numbers and will return the XML result with an add one and subtract one operation",
-              grassLocation =False)
+from pywps import Process, LiteralInput, LiteralOutput
 
-         self.Input1 = self.addLiteralInput(identifier = "input1",
-                                            title = "Input1 number",
-                                            type=types.IntType,
-                                            default="100")
-         self.Input2= self.addLiteralInput(identifier="input2",
-                                           title="Input2 number",
-                                           type=types.IntType,
-                                          default="200")
-         self.Output1=self.addLiteralOutput(identifier="output1",
-                                            title="Output1 add 1 result")
-         self.Output2=self.addLiteralOutput(identifier="output2",title="Output2 subtract 1 result" )
-     def execute(self):
+import logging
+LOGGER = logging.getLogger("PYWPS")
 
-        self.Output1.setValue(int(self.Input1.getValue())+1)
-        self.Output2.setValue(int(self.Input1.getValue())-1)
-        return
+
+class Dummy(Process):
+    def __init__(self):
+        inputs = [
+            LiteralInput('input1', 'Input1 number',
+                         default='100', data_type='integer'),
+            LiteralInput('input2', 'Input2 number',
+                         default='200', data_type='integer'),
+        ]
+        outputs = [
+            LiteralOutput('output1', 'Output1 add 1 result',
+                          data_type='string'),
+            LiteralOutput('output2', 'Output2 substract 1 result',
+                          data_type='string'),
+        ]
+
+        super(Dummy, self).__init__(
+            self._handler,
+            identifier='dummyprocess',
+            title="Dummy Process",
+            version="2.0",
+            inputs=inputs,
+            outputs=outputs,
+            store_supported=True,
+            status_supported=True
+        )
+
+    def _handler(self, request, response):
+        LOGGER.debug("input1 %s", request.inputs['input1'][0].data)
+        LOGGER.debug("input2 %s", request.inputs['input2'][0].data)
+        response.outputs['output1'].data = request.inputs['input1'][0].data + 1
+        response.outputs['output2'].data = request.inputs['input2'][0].data - 1
+        return response
