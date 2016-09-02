5e892cb16448c23726d8dc4649e354dde9de865f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-09-02 17:13:50 +0200

    added abstracts

--- emu/processes/wps_inout.py
+++ emu/processes/wps_inout.py
@@ -18,23 +18,35 @@ class InOut(Process):
     def __init__(self):
         inputs = [
             LiteralInput('string', 'String', data_type='string',
+                         abstract='Enter a simple string.',
                          default="This is just a string"),
             LiteralInput('int', 'Integer', data_type='integer',
+                         abstract='Enter an integer number.',
                          default="7"),
             LiteralInput('float', 'Float', data_type='float',
+                         abstract='Enter a float number.',
                          default="3.14"),
+            # TODO: boolean default is not displayed in phoenix
             LiteralInput('boolean', 'Boolean', data_type='boolean',
-                         default='True'),
-            LiteralInput('time', 'Time', data_type='time'),
+                         abstract='Make your choice :)',
+                         default='1'),
+            LiteralInput('time', 'Time', data_type='time',
+                         abstract='Enter a time like 2016-09-02.',
+                         default='2016-09-02'),
             LiteralInput('string_choice', 'String Choice', data_type='string',
+                         abstract='Choose one item form list.',
                          allowed_values=['one', 'two', 'three'],
                          default='two'),
-            LiteralInput('string_multiple_choice', 'String Multiple Choice', data_type='string',
+            LiteralInput('string_multiple_choice', 'String Multiple Choice',
+                         abstract='Choose one or two items from list.',
+                         data_type='string',
                          allowed_values=['one', 'two', 'three', 'four'],
                          min_occurs=1, max_occurs=2,
                          default='three'),
             # BoundingBoxInput('bbox', 'Bounding Box', ['epsg:4326', 'epsg:3035']),
             ComplexInput('text', 'Text',
+                         abstract='Enter a URL pointing to a text document (optional)',
+                         min_occurs=0,
                          supported_formats=[Format('text/plain')])
 
 
@@ -79,7 +91,8 @@ class InOut(Process):
             [inpt.data for inpt in request.inputs['string_multiple_choice']])
         # TODO: bbox is not working
         # response.outputs['bbox'].data = request.inputs['bbox'][0].data
-        response.outputs['text'].output_format = FORMATS.TEXT
         # TODO: how to copy file?
-        response.outputs['text'].file = request.inputs['text'][0].file
+        if 'text' in request.inputs:
+            response.outputs['text'].output_format = FORMATS.TEXT
+            response.outputs['text'].file = request.inputs['text'][0].file
         return response
