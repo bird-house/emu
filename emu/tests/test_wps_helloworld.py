fc99fd58961d5c450fbfde63db36766f75c1e17c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-10-18 14:11:04 +0200

    remove old helloworld test

--- emu/tests/test_wps_helloworld.py
+++
@@ -1,8 +0,0 @@
-from emu.tests.common import WpsTestClient, assert_response_success
-
-def test_wps_helloworld():
-    wps = WpsTestClient()
-    datainputs = "[user=LovelySugarBird]"
-    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='helloworld',
-                   datainputs=datainputs)
-    assert_response_success(resp)
