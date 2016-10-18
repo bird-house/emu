b0068862246ae9af6702770ba0de525ada8ca1ac
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-10-18 14:17:46 +0200

    update chomsky test

--- emu/tests/test_wps_chomsky.py
+++ emu/tests/test_wps_chomsky.py
@@ -1,8 +1,14 @@
-from emu.tests.common import WpsTestClient, assert_response_success
+from pywps import Service
+
+from emu.tests.common import client_for, assert_response_success
+from emu.processes.wps_chomsky import Chomsky
+
 
 def test_wps_chomsky():
-    wps = WpsTestClient()
-    datainputs = "[times=10]"
-    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='chomsky',
-                   datainputs=datainputs)
+    client = client_for(Service(processes=[Chomsky()]))
+    datainputs = "times=10"
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0',
+        identifier='chomsky',
+        datainputs=datainputs)
     assert_response_success(resp)
