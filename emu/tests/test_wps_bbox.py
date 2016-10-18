e87a2be4ef4423e973cd4cb6e6ad1263fb6a7174
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-10-18 14:18:06 +0200

    update bbox test

--- emu/tests/test_wps_bbox.py
+++ emu/tests/test_wps_bbox.py
@@ -1,8 +1,17 @@
-from emu.tests.common import WpsTestClient, assert_response_success
+import pytest
 
+from pywps import Service
+
+from emu.tests.common import client_for, assert_response_success
+from emu.processes.wps_bbox import Box
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_wps_bbox():
-    wps = WpsTestClient()
-    datainputs = "[bbox=101,42,110,46]"
-    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='bbox',
-                   datainputs=datainputs)
+    client = client_for(Service(processes=[Box()]))
+    datainputs = "bbox=101,42,110,46"
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0',
+        identifier='bbox',
+        datainputs=datainputs)
     assert_response_success(resp)
