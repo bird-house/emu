a7d733819bfa91666a1b45240be96f2fc6222f05
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-10-18 14:09:55 +0200

    added hello test

---
+++ emu/tests/test_wps_hello.py
@@ -0,0 +1,14 @@
+from pywps import Service
+
+from emu.tests.common import client_for, assert_response_success
+
+from emu.processes.wps_hello import Hello
+
+
+def test_wps_hello():
+    client = client_for(Service(processes=[Hello()]))
+    datainputs = "name=LovelySugarBird"
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0', identifier='hello',
+        datainputs=datainputs)
+    assert_response_success(resp)
