5b6277ce1e99e49582d9da7f7c9e5b851dff1cf0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-10-20 15:39:03 +0200

    pep8

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 reqs = [line.strip() for line in open('requirements/deploy.txt')]
 
-classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
-        ]
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: POSIX',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Atmospheric Science',
+]
 
 setup(name='emu',
       version='0.5.0',
@@ -25,15 +25,14 @@ setup(name='emu',
       author='Birdhouse',
       author_email='',
       url='https://github.com/bird-house/emu',
-      license = "Apache License v2.0",
+      license="Apache License v2.0",
       keywords='wps pywps malleefowl emu conda birdhouse',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       test_suite='emu',
       install_requires=reqs,
-      entry_points = {
-          'console_scripts': [
-              ]}     
-      ,
+      entry_points={
+          'console_scripts': []
+      },
       )
