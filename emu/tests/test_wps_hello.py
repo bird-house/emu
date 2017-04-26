fba5bf83bb37306c9b92084e7355551c2ee62b43
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-04-26 14:04:42 +0200

    fixed say_hello test

--- emu/tests/test_wps_hello.py
+++ emu/tests/test_wps_hello.py
@@ -2,11 +2,11 @@ from pywps import Service
 from pywps.tests import assert_response_success
 
 from .common import client_for
-from emu.processes.wps_hello import Hello
+from emu.processes.wps_say_hello import SayHello
 
 
 def test_wps_hello():
-    client = client_for(Service(processes=[Hello()]))
+    client = client_for(Service(processes=[SayHello()]))
     datainputs = "name=LovelySugarBird"
     resp = client.get(
         service='WPS', request='Execute', version='1.0.0', identifier='hello',
