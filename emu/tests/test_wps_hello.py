d9879029bfe02beefbf7f557897384212d58a42e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-11 18:13:32 +0200

    test wps_hello_again added

--- emu/tests/test_wps_hello.py
+++ emu/tests/test_wps_hello.py
@@ -12,3 +12,17 @@ def test_wps_hello():
         service='WPS', request='Execute', version='1.0.0', identifier='hello',
         datainputs=datainputs)
     assert_response_success(resp)
+
+
+def test_wps_hello_again():
+    """Example of how to debug this process, running outside a PyWPS instance.
+    """
+    hello = SayHello()
+    (request, response) = hello.build_request_response()
+    literal_in = hello.inputs[0]
+    literal_in.data = 'Alice'
+    request.inputs["name"].append(literal_in)
+    hello._handler(request, response)
+
+    assert response.outputs["output"].data == 'Hello Alice'
+    print("All good!")
