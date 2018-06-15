ab8eb0bf791cd591e63a96a6166a12bda159cf82
David Huard <david.huard@gmail.com>
2018-06-15 10:50:46 -0400

    Added process returning netcdf and json files to test clients (#43)
    Fix #42: Added process returning netcdf and json files to test clients

---
+++ emu/processes/wps_output_formats.py
@@ -0,0 +1,46 @@
+"""
+Process returning a variety of output file formats to help test clients.
+
+Author: David Huard
+"""
+import os
+from pywps import Process, ComplexOutput
+from pywps import FORMATS
+from eggshell.config import Paths
+import emu
+import logging
+LOGGER = logging.getLogger("PYWPS")
+paths = Paths(emu)
+
+
+class OutputFormats(Process):
+    def __init__(self):
+        inputs = []
+        outputs = [
+            ComplexOutput('netcdf', 'netCDF dummy output file.',
+                          abstract="A very small test netCDF file. ",
+                          as_reference=True,
+                          supported_formats=[FORMATS.NETCDF, ]),
+            ComplexOutput('json', 'json dummy output file.',
+                          abstract="A very small test json file. ",
+                          as_reference=True,
+                          supported_formats=[FORMATS.JSON, ]),
+        ]
+
+        super(OutputFormats, self).__init__(
+            self._handler,
+            identifier='output_formats',
+            title="Return different output formats. ",
+            abstract="Dummy process returning various output file formats.",
+            version="2.0",
+            inputs=inputs,
+            outputs=outputs,
+            store_supported=True,
+            status_supported=True
+        )
+
+    @staticmethod
+    def _handler(request, response):
+        response.outputs['netcdf'].file = os.path.join(paths.data, 'dummy.nc')
+        response.outputs['json'].file = os.path.join(paths.data, 'dummy.json')
+        return response
---
+++ tests/test_output_formats.py
@@ -0,0 +1,13 @@
+from pywps import Service
+from pywps.tests import assert_response_success
+
+from .common import client_for
+from emu.processes.wps_output_formats import OutputFormats
+
+
+def test_wps_output_formats():
+    client = client_for(Service(processes=[OutputFormats()]))
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0',
+        identifier='output_formats',)
+    assert_response_success(resp)
