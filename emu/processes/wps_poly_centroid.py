9cd706717fee76f278fe83abd28cc9c0b00eea4a
David Huard <david.huard@gmail.com>
2018-07-30 11:35:20 -0400

    Added process requiring an application/xml+gml mimetype input (#45)
    * added process requiring a xml+gml mimetype input
    
    * fix pep8 and test_caps

---
+++ emu/processes/wps_poly_centroid.py
@@ -0,0 +1,60 @@
+"""
+Process using an application/xml+gml input. Used to test UI interactions.
+
+Author: David Huard
+"""
+from pywps import Process, ComplexInput, LiteralOutput
+from pywps import FORMATS
+import logging
+LOGGER = logging.getLogger("PYWPS")
+
+
+class PolyCentroid(Process):
+    def __init__(self):
+        inputs = [
+            ComplexInput('polygon', 'Region definition',
+                         abstract="A polygon defining a region.",
+                         supported_formats=[FORMATS.GML, ]),
+        ]
+        outputs = [
+            LiteralOutput('centroid', 'The centroid of the polygon geometry.',
+                          abstract="The coordinates of the polygon's approximate centroid.",)
+        ]
+
+        super(PolyCentroid, self).__init__(
+            self._handler,
+            identifier='poly_centroid',
+            title="Approximate centroid of a polygon.",
+            abstract="Return the polygon's centroid coordinates. If the geometry contains multiple polygons, "
+                     "only the centroid of the first one will be computed. Do not use for serious computations"
+                     ", this is only a test process and uses a crude approximation. ",
+            version="1.0",
+            inputs=inputs,
+            outputs=outputs,
+            store_supported=True,
+            status_supported=True
+        )
+
+    @staticmethod
+    def _handler(request, response):
+        from xml.etree import ElementTree
+        fn = request.inputs['polygon'][0].file
+
+        ns = {'gml': 'http://www.opengis.net/gml'}
+        poly = ElementTree.parse(fn)
+
+        # Find the first polygon in the file.
+        e = poly.find('.//gml:Polygon', ns)
+
+        # Get the coordinates
+        c = e.find('.//gml:coordinates', ns).text
+        coords = [tuple(map(float, p.split(','))) for p in c.split(' ')]
+
+        # Compute the average
+        n = len(coords)
+        x, y = zip(*coords)
+        cx = sum(x) / n
+        cy = sum(y) / n
+
+        response.outputs['centroid'].data = '{},{}'.format(cx, cy)
+        return response
---
+++ tests/test_wps_poly_centroid.py
@@ -0,0 +1,24 @@
+import pytest
+import os
+from pywps import Service
+from pywps.tests import assert_response_success
+
+from .common import client_for
+import emu
+from emu.processes.wps_poly_centroid import PolyCentroid
+from eggshell.config import Paths
+
+paths = Paths(emu)
+TESTS_HOME = os.path.abspath(os.path.dirname(__file__))
+cfgfiles = os.path.join(TESTS_HOME, 'test.cfg')
+
+
+def test_wps_poly_centroid():
+    client = client_for(Service(processes=[PolyCentroid(), ], cfgfiles=cfgfiles))
+    fn = os.path.join(TESTS_HOME, 'testdata', 'poly.xml')
+    datainputs = "polygon=files@xlink:href=file://{0}".format(fn)
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0',
+        identifier='poly_centroid',
+        datainputs=datainputs)
+    assert_response_success(resp)
