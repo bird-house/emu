8851e6afcb20f99d6889fd94ac5e6d7c24470046
MacPingu <cehbrecht@users.noreply.github.com>
2018-08-14 16:01:01 +0200

    fix sleep and wordcounter test (#53)

--- tests/test_wps_wordcounter.py
+++ tests/test_wps_wordcounter.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pywps import Service
 from pywps.tests import assert_response_success
 
@@ -5,10 +7,22 @@ from .common import client_for, resource_file
 from emu.processes.wps_wordcounter import WordCounter
 
 
-def test_wps_wordcount():
+def test_wps_wordcount_file():
     client = client_for(Service(processes=[WordCounter()]))
     datainputs = "text=@xlink:href=file://{0}".format(
         resource_file('alice-in-wonderland.txt'))
+    resp = client.get(
+        service='wps', request='execute', version='1.0.0',
+        identifier='wordcounter',
+        datainputs=datainputs)
+    assert_response_success(resp)
+
+
+@pytest.mark.online
+def test_wps_wordcount_href():
+    client = client_for(Service(processes=[WordCounter()]))
+    datainputs = "text=@xlink:href={0}".format(
+        "https://en.wikipedia.org/wiki/Web_Processing_Service")
     resp = client.get(
         service='wps', request='execute', version='1.0.0',
         identifier='wordcounter',
