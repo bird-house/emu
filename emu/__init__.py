0f948e613db44a33b54285d3c79d28e415b72d79
David Caron <dcaron05@gmail.com>
2018-11-30 16:57:12 -0500

    use a single `__version__.py` file instead

--- emu/__init__.py
+++ emu/__init__.py
@@ -1,3 +1,1 @@
 from .wsgi import application
-
-__version__ = '0.9.0'
--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-for line in open(os.path.join(here, 'emu', '__init__.py')):
-    if line.startswith('__version__'):
-        version = re.match('__version__ ?= ?(.+)', line).group(1)
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 reqs = [line.strip() for line in open('requirements.txt')]
 extra_reqs = [line.strip() for line in open('requirements_dev.txt')]
+
+# copied from `requests`
+about = {}
+with open(os.path.join(here, 'emu', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
 
 classifiers = [
     'Development Status :: 4 - Beta',
@@ -23,7 +25,7 @@ classifiers = [
 ]
 
 setup(name='emu',
-      version=version,
+      version=about['__version__'],
       description='WPS processes for testing and demo',
       long_description=README + '\n\n' + CHANGES,
       classifiers=classifiers,
