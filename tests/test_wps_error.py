7de76e609fa07fbe724d526341533ef2bc6fc2e8
Pingu Carsti <ehbrecht@dkrz.de>
2019-04-10 17:25:58 +0200

    added wps error test

---
+++ tests/test_wps_error.py
@@ -0,0 +1,15 @@
+from pywps import Service
+from pywps.tests import client_for
+
+from emu.processes.wps_error import ShowError
+
+
+def test_wps_error():
+    client = client_for(Service(processes=[ShowError()]))
+    datainputs = "message=tomorrow-is-another-day;nice=true"
+    resp = client.get(
+        "?service=WPS&request=Execute&version=1.0.0&identifier=hello&datainputs={}".format(
+            datainputs))
+    # TODO: parse ows:Exception
+    # assert_response_success(resp)
+    # assert get_output(resp.xml) == {'output': "Hello LovelySugarBird"}
