61519027544f9defbee6b2b9c24d4cb97481b877
Pingu Carsti <ehbrecht@dkrz.de>
2019-04-10 17:19:07 +0200

    updated comments

--- emu/processes/wps_error.py
+++ emu/processes/wps_error.py
@@ -7,13 +7,24 @@ LOGGER = logging.getLogger("PYWPS")
 
 
 class ShowError(Process):
+    """Show a user friendly error message using :class:`ProcessError`.
+
+    An example request::
+
+        http://localhost:5000/wps? \
+            version=1.0.0& \
+            service=wps& \
+            request=Execute& \
+            identifier=show_error& \
+            DataInputs=message=bad-day;nice=true
+    """
     def __init__(self):
         inputs = [
             LiteralInput('message', 'Error Message', data_type='string',
                          abstract='Enter an error message that will be returned.',
                          default="This process failed intentionally.",
                          min_occurs=1,),
-            LiteralInput('be_nice', 'Be nice and show a friendly error message. Default: true',
+            LiteralInput('nice', 'Be nice and show a friendly error message. Default: true',
                          data_type='boolean',
                          default=True, ),
         ]
@@ -22,8 +33,9 @@ class ShowError(Process):
             self._handler,
             identifier='show_error',
             title='Show a WPS Error',
-            abstract='This process will fail intentionally with a WPS error message.',
+            abstract='This process will fail intentionally with a friendly WPS error message.',
             metadata=[
+                Metadata('PyWPS', 'https://pywps.org/'),
                 Metadata('Birdhouse', 'http://bird-house.github.io/'),
                 Metadata('User Guide', 'http://emu.readthedocs.io/en/latest/')],
             version='1.0',
@@ -38,7 +50,7 @@ class ShowError(Process):
         response.update_status('PyWPS Process started.', 0)
 
         LOGGER.info("wps_error started ...")
-        if request.inputs['be_nice'][0].data is True:
+        if request.inputs['nice'][0].data is True:
             raise ProcessError(request.inputs['message'][0].data)
         else:
             raise Exception("Sorry, we have no explanation for this error.")
