b0b2204e1c53920a17f942e12f8cde6857cf5435
f-PLT <pelletier.f@gmail.com>
2020-10-13 12:08:24 -0400

    Add supported formats to process output_formats
    Change to process handler and added zip version of dummy files.

--- emu/processes/wps_output_formats.py
+++ emu/processes/wps_output_formats.py
@@ -21,18 +21,18 @@ class OutputFormats(Process):
             ComplexOutput('netcdf', 'netCDF dummy output file.',
                           abstract="A very small test netCDF file. ",
                           as_reference=True,
-                          supported_formats=[FORMATS.NETCDF, ]),
+                          supported_formats=[FORMATS.NETCDF, FORMATS.ZIP]),
             ComplexOutput('json', 'json dummy output file.',
                           abstract="A very small test json file. ",
                           as_reference=False,
-                          supported_formats=[FORMATS.JSON, ]),
+                          supported_formats=[FORMATS.JSON, FORMATS.ZIP, FORMATS.TEXT]),
         ]
 
         super(OutputFormats, self).__init__(
             self._handler,
             identifier='output_formats',
-            title="Return different output formats. ",
-            abstract="Dummy process returning various output file formats.",
+            title="Return different output formats.",
+            abstract="Dummy process returning various output file formats. Output format can be specified between supported formats",
             version="2.0",
             inputs=inputs,
             outputs=outputs,
@@ -44,8 +44,17 @@ class OutputFormats(Process):
     def _handler(request, response):
         response.update_status('PyWPS Process started.', 0)
 
-        response.outputs['netcdf'].file = os.path.join(DATA_DIR, 'dummy.nc')
-        response.outputs['json'].file = os.path.join(DATA_DIR, 'dummy.json')
+        netcdf_out = response.outputs['netcdf']
+        if netcdf_out.data_format.mime_type == FORMATS.ZIP.mime_type:
+            netcdf_out.file = os.path.join(DATA_DIR, 'dummy_nc.zip')
+        else:
+            netcdf_out.file = os.path.join(DATA_DIR, 'dummy.nc')
+
+        json_out = response.outputs['json']
+        if json_out.data_format.mime_type == FORMATS.ZIP.mime_type:
+            json_out.file = os.path.join(DATA_DIR, 'dummy_json.zip')
+        else:
+            json_out.file = os.path.join(DATA_DIR, 'dummy.json')
 
         response.update_status('PyWPS Process completed.', 100)
         return response
