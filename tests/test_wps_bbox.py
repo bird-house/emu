3dbd0718d8a8a987639b19ea12f30ed3f342f647
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-11-18 13:35:44 +0100

    updated bbox test

--- tests/test_wps_bbox.py
+++ tests/test_wps_bbox.py
@@ -1,13 +1,13 @@
 from pywps import Service
 from pywps.tests import assert_response_success
 
-from .common import client_for
+from .common import client_for, get_output
 from emu.processes.wps_bbox import Box
 
 
 def test_wps_bbox_1():
     client = client_for(Service(processes=[Box()]))
-    datainputs = "bbox=101,42,110,46"
+    datainputs = "bbox=20,42,24,46"
 
     resp = client.get(
         service='WPS', request='Execute', version='1.0.0',
@@ -15,6 +15,8 @@ def test_wps_bbox_1():
         datainputs=datainputs)
     # print(resp.data)
     assert_response_success(resp)
+    assert '<ows:LowerCorner> 20.0  42.0 </ows:LowerCorner>' in str(resp.data)
+    assert '<ows:UpperCorner> 24.0  46.0 </ows:UpperCorner>' in str(resp.data)
 
 
 def test_wps_bbox_2():
@@ -26,3 +28,5 @@ def test_wps_bbox_2():
         datainputs=datainputs)
     # print(resp.data)
     assert_response_success(resp)
+    assert '<ows:LowerCorner> 46.0  102.0 </ows:LowerCorner>' in str(resp.data)
+    assert '<ows:UpperCorner> 47.0  103.0 </ows:UpperCorner>' in str(resp.data)
