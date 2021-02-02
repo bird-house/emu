4640b75fdb794e29cb6e7bdc03a6697d8f9f3483
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-02-02 17:50:04 -0500

    Make ultimate question ever more ultimate

--- emu/processes/wps_ultimate_question.py
+++ emu/processes/wps_ultimate_question.py
@@ -12,7 +12,7 @@ class UltimateQuestion(Process):
             identifier='ultimate_question',
             version='2.0',
             title='Answer to the ultimate question',
-            abstract='This process gives the answer to the ultimate question of "What is the meaning of life?"',
+            abstract='This process gives the answer to the ultimate question of life, the universe, and everything.',
             profile='',
             metadata=[Metadata('Ultimate Question'), Metadata('What is the meaning of life')],
             inputs=inputs,
@@ -24,9 +24,11 @@ class UltimateQuestion(Process):
     @staticmethod
     def _handler(request, response):
         import time
-        response.update_status('PyWPS Process started.', 0)
-
         sleep_delay = .1
+        response.update_status('PyWPS Process started.', 0)
+        time.sleep(sleep_delay)
+
+        response.update_status("Contacting the Deep Thought supercomputer.", 10)
         time.sleep(sleep_delay)
         response.update_status('Thinking...', 20)
         time.sleep(sleep_delay)
