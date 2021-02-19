a27332ccc57a2f2049a86e6895f256ffc3d5fb53
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-02-19 16:44:21 -0500

    Slight modification to test_wps_geo.py

--- tests/test_wps_geo.py
+++ tests/test_wps_geo.py
@@ -1,14 +1,17 @@
 from pywps import Service
 from pywps.tests import assert_response_success
 from emu.processes.wps_geodata import GeoData
-from .common import client_for, CFG_FILE
+from .common import client_for, CFG_FILE, get_output
 
 
-def test_wps_geodata():
+class TestGeoData:
+    def test_wps_geodata(self):
 
-    client = client_for(Service(processes=[GeoData()], cfgfiles=CFG_FILE))
+        client = client_for(Service(processes=[GeoData()], cfgfiles=CFG_FILE))
 
-    resp = client.get(
-        service='wps', request='execute', version='1.0.0',
-        identifier='geodata')
-    assert_response_success(resp)
+        resp = client.get(
+            service='wps', request='execute', version='1.0.0',
+            identifier='geodata', asobj=True)
+
+        assert_response_success(resp)
+        assert set(get_output(resp.xml).keys()) == {"vector", "raster"}
